1b0e904da93840a592ae8c322b9160001f023aaf
Adam CHADER <adam@vm-adam.int-evry.fr>
2022-03-24 14:56:17 +0100

    ça marche ?

--- run.sh
+++ run.sh
@@ -3,5 +3,9 @@
 if [ "$1" = "noblk" ]; then
 	spark_noblk/bin/spark-submit --class WordCount --master local[*] spark_run/run.jar > output 2>&1 ; grep "Time:" output 
 else
-	systemd-run --scope -p MemoryHigh=800M numactl --preferred=0 spark_blk/bin/spark-submit --class WordCount --master local[*] spark_run/run.jar > output 2>&1 ; grep "Time:" output
+	if [ "$1" = "blk" ]; then
+		systemd-run --scope -p MemoryHigh=800M numactl --preferred=0 spark_blk/bin/spark-submit --class WordCount --master local[*] spark_run/run.jar > output 2>&1 ; grep "Time:" output
+	else
+		systemd-run --scope -p MemoryHigh=800M numactl --preferred=0 spark_altblk/bin/spark-submit --class WordCount --master local[*] spark_run/run.jar > output 2>&1 ; grep "Time:" output
+	fi
 fi
